ca25899687d1fa6c8ba726ebc2fc2003990329c1
bandit1717 <rstupart@hotmail.com>
2024-07-15 15:41:56 -0400

    Add files via upload

---
+++ O365-user-licenses.ps1
@@ -0,0 +1,14 @@
+﻿#all user licenses
+$user = "user@userdomain.com"
+
+Import-Module Microsoft.Graph.Users
+
+Connect-Graph -Scopes User.ReadWrite.All, Organization.Read.All
+
+
+
+Get-mguserlicesedetail -userid $user
+
+(Get-MgUserLicenseDetail -UserId $user -Property ServicePlans).ServicePlans
+
+disconnect-graph
---
+++ calendar-setting-o365-on-prem.ps1
@@ -0,0 +1,12 @@
+﻿Connect-ExchangeOnline 
+
+$user = user@userdomain.com
+$user2 = $user+':\calendar'
+
+Get-exomailboxFolderpermission -Identity $user2 |ft
+ 
+# allow user to edit all items in calendar
+set-mailboxFolderpermission -Identity $user2 -User neeedingaccess@userdomain.com -AccessRights publishingeditor
+
+
+Disconnect-ExchangeOnline -Confirm:$false
