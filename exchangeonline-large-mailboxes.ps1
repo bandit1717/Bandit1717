3e02154e4748220262780dc5a29a22e5abc5c442
bandit1717 <rstupart@hotmail.com>
2024-07-15 16:00:09 -0400

    Add files via upload

---
+++ exchangeonline-large-mailboxes.ps1
@@ -0,0 +1,13 @@
+﻿# List huge mailboxes 
+
+
+connect-exchangeonline
+
+#user mailboxes
+Get-EXOMailbox -ResultSize Unlimited | Get-EXOMailboxStatistics | Where-Object { $_.TotalItemSize.Value.ToBytes() -gt "50GB" } | Select-Object DisplayName, primarysmtpaddress, ItemCount, TotalItemSize | Sort-Object TotalItemSize -Descending
+
+
+#shared mailboxes
+Get-EXOMailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited | Get-EXOMailboxStatistics | Where-Object { $_.TotalItemSize.Value.ToBytes() -gt "50GB" } | Select-Object DisplayName, primarysmtpaddress, ItemCount, TotalItemSize | Sort-Object TotalItemSize -Descending
+
+
